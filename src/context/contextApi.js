e3d58f25ab5264cd257c3e2186b3b801f8151884
unknown <myself.suraj0@gmail.com>
2023-07-19 16:41:04 +0530

    Youtube Responsive Header Completed using Tailwind CSS

---
+++ src/context/contextApi.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const AppContext = React.createContext();
+
+const AppContextProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResult, setSearchResult] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [mobileMenu, setMobileMenu] = useState(null);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+    const fetchSelectedCategoryData = async (query) => {
+        setLoading(true);
+        try {
+            const { contents } = await fetchDataFromApi(`search/?q=${query}`);
+            console.log(contents);
+            setSearchResult(contents);
+        } catch (error) {
+            console.log(error);
+        }
+        setLoading(false);
+    };
+
+    return (
+        <AppContext.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResult,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export default AppContextProvider;
